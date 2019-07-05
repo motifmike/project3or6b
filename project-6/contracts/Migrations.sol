8d3a704544cb8a68aff61d5126cb098aad149716
Mike <mjohanson@gesdate.com>
2019-07-05 15:21:57 -0700

    migrations

--- project-6/contracts/Migrations.sol
+++ project-6/contracts/Migrations.sol
@@ -1,23 +1,22 @@
-pragma solidity ^0.5.8;
+pragma solidity 0.5.8;
+
 
 contract Migrations {
   address public owner;
-  uint public last_completed_migration;
+  uint public lastCompletedMigration;
 
-  constructor() public {
-    owner = msg.sender;
-  }
+  constructor() public { owner = msg.sender; }
 
-  modifier restricted() {
+  modifier restricted {
     if (msg.sender == owner) _;
   }
 
   function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+    lastCompletedMigration = completed;
   }
 
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
+  function upgrade(address newAddress) public restricted {
+    Migrations upgraded = Migrations(newAddress);
+    upgraded.setCompleted(lastCompletedMigration);
+  }  
 }
